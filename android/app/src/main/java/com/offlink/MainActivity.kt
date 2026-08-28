eea06f00b97af337afb5114102c75caa64f3ee7f
gurprit <gurprit.sahota@gmail.com>
2026-08-28 11:43:48 +0100

    Build B: respect Android system safe areas

--- android/app/src/main/java/com/offlink/MainActivity.kt
+++ android/app/src/main/java/com/offlink/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.offlink
 
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -12,6 +16,29 @@ class MainActivity : ReactActivity() {
    * rendering of the component.
    */
   override fun getMainComponentName(): String = "Offlink"
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    val contentView = findViewById<View>(android.R.id.content)
+
+    ViewCompat.setOnApplyWindowInsetsListener(contentView) { view, windowInsets ->
+      val safeInsets = windowInsets.getInsets(
+        WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout(),
+      )
+
+      view.setPadding(
+        safeInsets.left,
+        safeInsets.top,
+        safeInsets.right,
+        safeInsets.bottom,
+      )
+
+      windowInsets
+    }
+
+    ViewCompat.requestApplyInsets(contentView)
+  }
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
