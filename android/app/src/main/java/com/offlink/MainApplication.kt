71381cbcf827e568c65e3efa74914d36467427e2
gurprit <gurprit.sahota@gmail.com>
2026-09-13 00:56:54 +0100

    Wire heading package into Android app

--- android/app/src/main/java/com/offlink/MainApplication.kt
+++ android/app/src/main/java/com/offlink/MainApplication.kt
@@ -12,6 +12,7 @@ import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
 import com.offlink.gatt.OfflinkGattPackage
+import com.offlink.heading.OfflinkHeadingPackage
 
 class MainApplication : Application(), ReactApplication {
 
@@ -21,6 +22,7 @@ class MainApplication : Application(), ReactApplication {
             PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
               add(OfflinkGattPackage())
+              add(OfflinkHeadingPackage())
             }
 
         override fun getJSMainModuleName(): String = "index"
